238ffccad94f8dc6cfd7fe8d89aafcf1fffaf105
Ethan Chang <chang115@bu.edu>
2024-09-19 15:17:56 -0400

    urls update again

--- hw/urls.py
+++ hw/urls.py
@@ -1,12 +1,13 @@
 ## hw/urls.py
-##description: URL patterns for the hw app
+## description: the app-specific URLS for the hw application
 
 from django.urls import path
 from django.conf import settings
 from . import views
 
-# create a list of URLS for this app:
+# create a list of URLs for this app:
 urlpatterns = [
-    path(r'', views.home, name="home"), ## our first URL
+    path(r'', views.home, name="home"), 
     path(r'about', views.about, name="about"), ## new
+
 ]
